aaf3334b82d86a47e13a655d3001f1bc02d95413
Nkama Chukwuemeka <nkcemeka@gmail.com>
2026-06-16 11:38:12 +0200

    Add tensorboard logger

--- snap2midi/utils/train_utils.py
+++ snap2midi/utils/train_utils.py
@@ -1,5 +1,5 @@
 import pytorch_lightning as pl
-from pytorch_lightning.loggers import WandbLogger, CSVLogger
+from pytorch_lightning.loggers import WandbLogger, CSVLogger, TensorBoardLogger
 from lightning_fabric.loggers.logger import Logger
 
 def pl_logger(logger_name: str="csv", project_name: str="lightning_logs") -> Logger:
@@ -19,7 +19,8 @@ def pl_logger(logger_name: str="csv", project_name: str="lightning_logs") -> Log
     """
     logger_dict = {
         "csv": CSVLogger(".", name=project_name),
-        "wandb": WandbLogger(name=project_name)
+        "wandb": WandbLogger(name=project_name),
+        "tensorboard": TensorBoardLogger(name=project_name),
     }
 
     assert logger_name in logger_dict, "[ERROR] Recognized loggers are `csv` and `wandb`"
